b3bd0c2464bf8732fe5df17708ea97219d504853
doktor_justice <somiandras@gmail.com>
2018-03-01 17:34:26 +0100

    Add data upload to client
    Add POST request with sensor data to API endpoint

--- main.py
+++ main.py
@@ -6,12 +6,19 @@ from tsl2561 import TSL2561
 from luma.core.interface.serial import spi
 from luma.core.render import canvas
 from luma.oled.device import ssd1306
+
+from datetime import datetime
 import time
 import sys
-from datetime import datetime
+import os
+import requests
+
+serial = spi(gpio_DC=27, gpio_RST=22)
+device = ssd1306(serial)
 
 if __name__ == '__main__':
-    now = datetime.now()
+
+    now = datetime.now().isoformat()
     # Get temperature and humidity values from AM2320
     am2320 = AM2320(1)
     (temp, humi) = am2320.readSensor()
@@ -20,13 +27,29 @@ if __name__ == '__main__':
     tsl = TSL2561(debug=1)
     lux = tsl.lux()
 
-    serial = spi(gpio_DC=27, gpio_RST=22)
-    device = ssd1306(serial)
+    # Send data
+    url = os.environ['SERVER_URL'] + '/readings'
+    token = os.environ['TOKEN']
+    payload = {
+        'date': now,
+        'temp': temp,
+        'humi': humi,
+        'lux': lux
+    }
+    headers = {
+        'Authorization': 'Bearer ' + token,
+        'Content-type': 'application/json'
+    }
+    r = requests.post(url, headers=headers, json=payload)
 
-    with canvas(device) as draw:
-        string = ('{}:\nTemperature: {} C,\nHumidity: {}%,\nLuminosity: {} lux'
-                  .format(now, temp, humi, lux))
-        draw.text((0, 0), string, fill="white")
-    time.sleep(15)
-    print(string.replace('\n', ' '))
-    sys.exit(0)
+    if r.status_code == 200:
+        # Print on OLED screen
+        with canvas(device) as draw:
+            string = ('{}:\nTemperature: {} C,\nHumidity: {}%,\nLuminosity: {} lux'
+                      .format(now, temp, humi, lux))
+            draw.text((0, 0), string, fill="white")
+        time.sleep(15)
+        sys.exit(0)
+    else:
+        r.raise_for_status()
+        sys.exit(1)
